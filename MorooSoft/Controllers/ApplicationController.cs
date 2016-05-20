95b48b270b36b12b91589d15f6336d85e1a04506
moroo Lee <moroo.lee@gmail.com>
2016-05-20 16:19:57 +0900

    2016-05-20_Notebook

---
+++ MorooSoft/Controllers/ApplicationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MorooSoft.Controllers
+{
+    public class ApplicationController : Controller
+    {
+        // GET: Application
+        public ActionResult HostManager()
+        {
+            return View();
+        }
+    }
+}
---
+++ MorooSoft/Models/ApplicationViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MorooSoft.Models
+{
+    public class ApplicationViewModel
+    {
+        [Display(Name = "어플리케이션 이름")]
+        public string AppName { get; set; }
+
+        [Display(Name = "어플리케이션 버전")]
+        public string AppVersion { get; set; }
+    }
+}
