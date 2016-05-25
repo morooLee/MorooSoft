2388f3f7ad7402c3e4b3ef6a7258c447d92f449c
moroo Lee <moroo.lee@gmail.com>
2016-05-25 22:28:55 +0900

    2016-05-25_Notebook

--- MorooSoft/Controllers/ApplicationController.cs
+++ MorooSoft/Controllers/ApplicationController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,8 +10,16 @@ namespace MorooSoft.Controllers
     public class ApplicationController : Controller
     {
         // GET: Application
-        public ActionResult HostManager()
+        //public ActionResult HostManager()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult HostManager(String version)
         {
+            //FileVersionInfo myFI = FileVersionInfo.GetVersionInfo("~/Files/HostManager/64Bit/HostManager.exe");
+            FileVersionInfo myFI = FileVersionInfo.GetVersionInfo(@"C:\Users\moroo\OneDrive\문서\Visual Studio 2015\Projects\MorooSoft\MorooSoft\Files\HostManager\64Bit\HostManager.exe");
+            ViewBag.Message = myFI.FileVersion;
             return View();
         }
     }
