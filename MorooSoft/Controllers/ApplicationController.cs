8c2dac0c28f386c584bfef048da005406a7e66c0
이순한 <xellos82@hanmail.net>
2016-05-28 02:47:34 +0900

    2016-05-28_Desktop

--- MorooSoft/Controllers/ApplicationController.cs
+++ MorooSoft/Controllers/ApplicationController.cs
@@ -20,9 +20,30 @@ namespace MorooSoft.Controllers
             String path = Request.PhysicalApplicationPath + "/Files/HostManager/64Bit/HostManager.exe";
             FileVersionInfo myFI = FileVersionInfo.GetVersionInfo(path);
             //FileVersionInfo myFI = FileVersionInfo.GetVersionInfo(@"C:\Users\moroo\OneDrive\문서\Visual Studio 2015\Projects\MorooSoft\MorooSoft\Files\HostManager\64Bit\HostManager.exe");
-            ViewBag.Message = myFI.FileVersion;
+            
+            if (version != null)
+            {
+                return Json(new {version = myFI.FileVersion}, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                ViewBag.Message = myFI.FileVersion;
+            }
+            
             //ViewBag.Message = path;
             return View();
         }
+
+        //public JsonResult HostManager(String version)
+        //{
+        //    String path = Request.PhysicalApplicationPath + "/Files/HostManager/64Bit/HostManager.exe";
+        //    FileVersionInfo myFI = FileVersionInfo.GetVersionInfo(path);
+        //    //FileVersionInfo myFI = FileVersionInfo.GetVersionInfo(@"C:\Users\moroo\OneDrive\문서\Visual Studio 2015\Projects\MorooSoft\MorooSoft\Files\HostManager\64Bit\HostManager.exe");
+        //    ViewBag.Message = myFI.FileVersion;
+        //    //ViewBag.Message = path;
+
+        //    version = myFI.FileVersion;
+        //    return Json(version, JsonRequestBehavior.AllowGet);
+        //}
     }
 }
